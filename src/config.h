dc35ee3f200cc787030b96eb53bc8c8343938ad6
Quan Do <biquando@gmail.com>
2023-08-22 17:09:47 -0700

    Clarify -b usage info

--- src/config.h
+++ src/config.h
@@ -8,19 +8,19 @@
 "Create or configure a launchtab.\n"                                           \
 "\n"                                                                           \
 "Operations\n"                                                                 \
-"  -b, --backup     backup user's current launchtab to a file (defaults to \n" \
-"                    ~/.config/launchtab/launch.tab.backup)\n"                 \
-"  -c, --cleanup    clean up temp files\n"                                     \
-"  -e, --edit       edit user's launchtab\n"                                   \
-"  -h, --help       show this message\n"                                       \
-"  -l, --list       list user's launchtab\n"                                   \
-"  -R, --reload     reload user's launchtab\n"                                 \
-"  -r, --remove     remove user's launchtab\n"                                 \
+"  -b, --backup FILE  backup user's current launchtab to FILE (defaults to \n" \
+"                      ~/.config/launchtab/launch.tab.backup)\n"               \
+"  -c, --cleanup      clean up temp files\n"                                   \
+"  -e, --edit         edit user's launchtab\n"                                 \
+"  -h, --help         show this message\n"                                     \
+"  -l, --list         list user's launchtab\n"                                 \
+"  -R, --reload       reload user's launchtab\n"                               \
+"  -r, --remove       remove user's launchtab\n"                               \
 "\n"                                                                           \
 "Options\n"                                                                    \
-"  -d, --debug      show debug messages when installing\n"                     \
-"  -q, --quiet      suppress output to stdout/stderr (doesn't affect\n"        \
-"                    output from -d)\n"                                        \
+"  -d, --debug        show debug messages when installing\n"                   \
+"  -q, --quiet        suppress output to stdout/stderr (doesn't affect\n"      \
+"                      output from -d)\n"                                      \
 ""
 
 #define DEFAULT_EDITOR "nano"
